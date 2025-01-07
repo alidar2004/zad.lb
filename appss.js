d8464b9c5db9e271f086e122907355864fddc7d0
alidar2004 <kaddoord2@gmail.com>
2025-01-07 08:07:20 +0200

    Add files via upload

--- appss.js
+++ appss.js
@@ -13,3 +13,19 @@ var swiper = new Swiper(".card_swiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
